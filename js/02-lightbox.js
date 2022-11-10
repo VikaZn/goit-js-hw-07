3912d73b0abde31878b0b5be2189c9640aeb70e8
VikaZn <zvvgoit@gmail.com>
2022-11-10 18:44:50 +0300

    hw-js-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+const galleryItemMarkup = creatGalleryItemMarkup(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+
+
+function creatGalleryItemMarkup(galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+         src=${preview}
+         alt=${description}/>
+        </a>
+    </div>`;
+    }).join("");
+
+};
+const modalLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
